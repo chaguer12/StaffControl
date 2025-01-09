f6abdf71f89dac59c0efdd399e518b17b8bd20a5
zoolotoui <chag.med.amine@gmail.com>
2025-01-09 09:33:37 +0100

    GDEAA-7 stat: working on a modal

--- src/app/employee/employee-list/employee-list.component.ts
+++ src/app/employee/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmployeeService,Employee } from '../employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -8,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrl: './employee-list.component.css'
 })
 export class EmployeeListComponent {
+  constructor(private employeeService: EmployeeService) { }
+  employees: Employee[] = [];
+
+  ngOnInit(): void {
+    this.fetchEmployees();
+  }
+
+  fetchEmployees(): void {
+    this.employeeService.getEmployees();
+  }
+  displayAddEmployeeForm(): void {
+    // Placeholder for showing an add employee form.
+    alert('Add Employee Form Logic Goes Here');
+  }
+  editEmployee(employee: Employee): void {
+    // Placeholder for editing an employee.
+    alert(`Edit Employee: ${employee.fullName}`);
+  }
+  deleteEmployee(id: number): void {
+    if (confirm('Are you sure you want to delete this employee?')) {
+      this.employeeService.deleteEmployee(id);
+      this.fetchEmployees();
+    }
+  }
 
 }
